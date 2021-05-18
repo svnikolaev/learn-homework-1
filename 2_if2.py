20c224a4f68979e1636fcf7b950cbfc6d4855029
Serg Nikolaev <kndnou@yandex.ru>
2021-05-18 17:42:44 +1100

    implement compare_strings function

--- 2_if2.py
+++ 2_if2.py
@@ -15,12 +15,31 @@
 
 """
 
+def compare_strings(first_string, second_string):
+    # лучше делать return result в конце функции, или return value после каждого if/elif ?
+    result = None
+    # что лучше возвращать когда конфигурация переданных параметров не описана в спецификации?
+    are_strings = type(first_string) == type(second_string) == type(str())
+    if not are_strings:
+        return 0
+    if first_string == second_string:
+        result = 1
+    elif len(first_string) > len(second_string):
+        result = 2
+    elif first_string != second_string and second_string == "learn":
+        result = 3
+    return result
+
 def main():
-    """
-    Эта функция вызывается автоматически при запуске скрипта в консоли
-    В ней надо заменить pass на ваш код
-    """
-    pass
+    print(compare_strings("a", 2)) #=> 0
+    print(compare_strings(1, "bb")) #=> 0
+    print(compare_strings(1, 2)) #=> 0
+    print(compare_strings("aa", "bb")) #=> None
+    print(compare_strings("aa", "aa")) #=> 1
+    print(compare_strings("aaa", "aa")) #=> 2
+    print(compare_strings("aaa", "learn")) #=> 3
+    print(compare_strings("learnlearn", "learn")) #=> 2
+    print(compare_strings("learn", "learn")) #=> 1
     
 if __name__ == "__main__":
     main()
