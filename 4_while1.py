f296a263803a2c164ae73d2d4c094aeedc3d00e1
Serg Nikolaev <kndnou@yandex.ru>
2021-05-19 06:02:52 +1100

    implemented hello_user function

--- 4_while1.py
+++ 4_while1.py
@@ -4,18 +4,21 @@
 
 Цикл while: hello_user
 
-* Напишите функцию hello_user(), которая с помощью функции input() спрашивает 
+* [x] Напишите функцию hello_user(), которая с помощью функции input() спрашивает 
   пользователя “Как дела?”, пока он не ответит “Хорошо”
    
 """
 
-
 def hello_user():
-    """
-    Замените pass на ваш код
-    """
-    pass
+    user_input_fine = False
+    # где-то слышал, что while True - не очень хорошо использовать, почему?
+    while not user_input_fine:
+        user_input = input("Как дела?\n")
+        if user_input.lower() == "хорошо":
+            user_input_fine = True
+            print('Ну пока')
+            # break
+    print()
 
-    
 if __name__ == "__main__":
     hello_user()
