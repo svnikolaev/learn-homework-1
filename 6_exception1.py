cd7ebaecfd3d978d613279cb37e56ed555f95ff5
Serg Nikolaev <kndnou@yandex.ru>
2021-05-19 22:49:35 +1100

    added exception KeyboardInterrupt handling

--- 6_exception1.py
+++ 6_exception1.py
@@ -4,17 +4,25 @@
 
 Исключения: KeyboardInterrupt
 
-* Перепишите функцию hello_user() из задания while1, чтобы она 
+* [x] Перепишите функцию hello_user() из задания while1, чтобы она 
   перехватывала KeyboardInterrupt, писала пользователю "Пока!" 
   и завершала работу при помощи оператора break
     
 """
 
 def hello_user():
-    """
-    Замените pass на ваш код
-    """
-    pass
+    user_input_fine = False
+    while not user_input_fine:
+        try:
+            user_input = input("Как дела?\n")
+            if user_input.lower() == "хорошо":
+                user_input_fine = True
+                print('Ну пока')
+        except KeyboardInterrupt:
+            print()
+            print("Пока!")
+            break
+            
     
 if __name__ == "__main__":
     hello_user()
